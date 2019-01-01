07fc6b63d51b8c946a0310ae2652d3e1ba465983
Herts <lhz007563@gmail.com>
2019-01-02 00:01:08 +0800

    Circular Sequential Queue Implement #13

---
+++ 3-Queue/cirsqqueue.cpp
@@ -0,0 +1,38 @@
+#define MAX_SIZE 50
+
+typedef char Elemtype;
+
+struct SqQueue
+{
+	Elemtype data[MAX_SIZE];
+	int front, rear;
+}
+
+
+void InitQueue(SqQueue *&q)
+{
+	q = new SqQueue;
+	q->front = q->rear = -1;
+}
+
+void DestroyQueue(SqQueue *&q)
+{
+	delete q;
+}
+
+bool QueueEmpty(SqQueue *q)
+{
+	return (q->front == q->rear);
+}
+
+bool EnQueue(SqQueue *&q, Elemtype e)
+{
+	if( (q->rear+1) % MAX_SIZE == q->front)
+		return false;
+
+	q->front = (q->front + 1) % MAX_SIZE;
+	e = q->data[q->front];
+	return true;
+}
+
+
